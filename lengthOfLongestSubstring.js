d767a4fe70eddbf6fb0627078fb122f493da898c
Akash Salunke <akash.salunke@ip-192-168-0-28.eu-west-1.compute.internal>
2025-11-11 16:40:20 +0530

    added program

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,29 @@
+var lengthOfLongestSubstring = function(s) {
+    let store = new Set();
+    let counter = 0;
+    let leftPtr = 0;
+    let rightPtr = 0;
+
+    while (rightPtr < s.length) {
+        const letter = s[rightPtr];
+
+        if (!store.has(letter)) {
+            store.add(letter);
+            counter = Math.max(counter, store.size)
+            rightPtr++;
+        } else {
+            store.delete(s[leftPtr]);
+            leftPtr++;
+        }
+    }
+    return counter;
+
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+
+// Sliding window
+// both ptr starts at same letter
+// if we see letter first time then we add it to set and increase right ptr.
+// also update counter to max of set size and current counter
+// if duplicate element then we delete from left ptr and increment leftPtr
